b2c59c3f91c58063221804d84ee4ee01136059b6
Gatlin Newhouse <gatlinnewhouse@gmail.com>
2021-04-21 11:57:58 -0700

    untested
    unsure if works, test sometime.
    
    also where does auth token go for odata stuff, look into

---
+++ pyhubb.py
@@ -0,0 +1,23 @@
+import pyodata
+import requests
+import json
+
+class HubbSite :
+    def __init__(self):
+        print("Apply for Hubb API Access here: https://apirequest.hubb.me/")
+    
+    def authHandshake(clientID: string, clientSecret: string, scope: string) -> string,string,string:
+        url = 'https://ngapi.hubb.me/auth/token'
+        creds = {'client_id': clientID, 'client_secret': clientSecret, 'scope': scope}
+        res = requests.post(url, data = creds, headers = {"Content-Type": "application/x-www-form-urlencoded"})
+        resp = json.load(res)
+        for p in resp:
+            accessToken = (p['access_token'])
+            tokenType = (p['token_type'])
+            expiry = (p['expires_in'])
+        return acessToken, tokenType, expiry
+    
+    def odataURL(serviceURL: string) -> pyodata.Client :
+        SERVICE_URL = serviceURL
+        HTTP_LIB = requests.Session()
+        return pyodata.Client(SERVICE_URL, HTTP_LIB)
