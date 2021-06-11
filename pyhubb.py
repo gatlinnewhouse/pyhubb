ce30712f88429846e32a0fee67312718c2355e26
Gatlin Newhouse <gatlinnewhouse@gmail.com>
2021-06-11 13:24:00 -0700

    Struggling with CLI for Library
    See issue #2, and here: https://github.com/uc-cdis/pypfb/blob/master/src/pfb/cli.py

--- pyhubb.py
+++ pyhubb.py
@@ -1,7 +1,51 @@
 import requests
 import json
+import pickle
+import os
+import time
 
-class client :
+# Pickling to cache data while CLI runs.
+def save_object(obj):
+    try:
+        with open("client.pickle", "wb") as f:
+            pickle.dump(obj, f)
+    except Exception as ex:
+        print("Error during pickling object (Possibly unsupported):", ex)
+
+# Open the pickle jar to grab the client info
+def load_object(filename):
+    try:
+        with open(filename, "rb") as f:
+            myinstance = pickle.load(f)
+        for k in myinstance.__dict__.keys():
+            setattr(client, k, getattr(myinstance, k))
+    except Exception as ex:
+        print("Error during unpickling object (Possibly unsupported):", ex)
+
+# Are the pickled client info objs expired?
+def expired_object(filename):
+    file_time = os.path.getmtime(filename)
+    if ((time.time() - file_time) / 3600 > 23):
+        # Should use this boolean to delete the pickle file and make a new one
+        return True
+    else:
+        # Should use this boolean to use the existing pickle
+        return False
+
+# Does this pickled obj exist?
+def exists(filename):
+    if os.path.exists(filename):
+        age = expired_object(filename)
+        if age == True:
+            os.remove(filename)
+            print("WARNING: Client data expired! Removed old pickled credentials. Run pyhubb_cli create\n")
+        else:
+            print("VERIFIED: Client data is not expired!\n")
+    else:
+        raise Exception("Client data pickle does not exist! Run pyhubb_cli create\n")
+
+class client(object):
+    # Create client
     def __init__(self, eventID, accessToken, tokenType, expiry, version):
         self.url = 'https://ngapi.hubb.me/api/' + str(version) + '/' + str(eventID) + '/'
         HTTP_LIB = requests.Session()
@@ -9,40 +53,43 @@ class client :
         self.tokenType = tokenType
         self.expiry = expiry
         self.headers = {'Authorization': 'Bearer' + accessToken, 'Content-Type': 'application/json'}
-    """ This allows you to expand a data field. """
-    """ If you're seeing a value of NULL or an empty array, 
-    but you expect there should be data, try "expanding" that value. """
+        save_object(self)
+    # This allows you to expand a data field.
+    # If you're seeing a value of NULL or an empty array, 
+    # but you expect there should be data, try "expanding" that value.
     def expand(self, section, fields) -> json:
+        exists('client.pickle')
+        load_object('client.pickle')
         details = requests.get(self.url + section + '?$expand=' + fields, headers=self.headers)
         print(details.json())
         return details.json()
-    """ This allows you to filter the data to only what you want to see. """
-    """ Only items where the expression evaluates to TRUE are included in the response. """
+    # This allows you to filter the data to only what you want to see.
+    # Only items where the expression evaluates to TRUE are included in the response.
     def filt(self, section, fields) -> json:
         details = requests.get(self.url + section + '?$filter=' + fields, headers=self.headers)
         print(details.json())
         return details.json()
-    """ This can be used to limit the data fields with are returned with a call. 
-    You can select only certain fields to come through. """
+    # This can be used to limit the data fields with are returned with a call. 
+    # You can select only certain fields to come through.
     def select(self, section, fields) -> json:
         details = requests.get(self.url + section + '?$select=' + fields, headers=self.headers)
         print(details.json())
         return details.json()
-    """ This will allow you to order the data by a certain parameter. """
+    # This will allow you to order the data by a certain parameter.
     def orderBy(self, section, fields) -> json:
         details = requests.get(self.url + section + '?$orderby=' + fields, headers=self.headers)
         print(details.json())
         return details.json()
-    """ This will allow you to limit the data that gets returned to a specific number. """
+    # This will allow you to limit the data that gets returned to a specific number.
     def top(self, section, fields) -> json:
         details = requests.get(self.url + section + '?$top=' + fields, headers=self.headers)
         print(details.json())
         return details.json()
-    """ myConference grabs all the events/sessions on a given users schedule """
-    """ It's recommended that you get the items for an individual user at a time. """
-    """ This will prevent the system from lagging as it tries to return an abundance of data. """
-    """ Note: The API is limited to only return 1000 items at a time. You can use the $top and
-    $skip parameters to create a loop that will get all items. """
+    # myConference grabs all the events/sessions on a given users schedule
+    # It's recommended that you get the items for an individual user at a time.
+    # This will prevent the system from lagging as it tries to return an abundance of data.
+    # Note: The API is limited to only return 1000 items at a time. You can use the $top and
+    # $skip parameters to create a loop that will get all items.
     def myConference(self, attendeeID) -> json:
         details = requests.get(self.url + 'Myconference?$filter=AttendeeId eq ' + attendeeID, headers=self.headers)
         print(details.json())
--- pyhubb_cli.py
+++ pyhubb_cli.py
@@ -1,45 +1,5 @@
 import pyhubb
-import pickle
 import fire
-import os
-import time
-
-# Pickling to cache data while CLI runs
-def save_object(obj):
-    try:
-        with open("client.pickle", "wb") as f:
-            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
-    except Exception as ex:
-        print("Error during pickling object (Possibly unsupported):", ex)
-
-# Open the pickle jar to grab the client info
-def load_object(filename):
-    try:
-        with open(filename, "rb") as f:
-            return pickle.load(f)
-    except Exception as ex:
-        print("Error during unpickling object (Possibly unsupported):", ex)
-
-# Are the pickled client info objs expired?
-def expired_object(filename):
-    file_time = os.path.getmtime(filename)
-    if ((time.time() - file_time) / 3600 > 23):
-        # Should use this boolean to delete the pickle file and make a new one
-        return True
-    else:
-        # Should use this boolean to use the existing pickle
-        return False
-
-def exists(filename):
-    if os.path.exists(filename):
-        age = expired_object(filename)
-        if age == True:
-            os.remove(filename)
-            print("WARNING: Client data expired! Removed old pickled credentials. Run pyhubb_cli init\n")
-        else:
-            print("VERIFIED: Client data is not expired!\n")
-    else:
-        raise Exception("Client data pickle does not exist! Run pyhubb_cli init\n")
 
 class pyhubbcli:
     """
@@ -48,7 +8,7 @@ class pyhubbcli:
     def __init__(self):
         pass
        
-    def init(eventID: str, accessToken: str, expiry:str, version: str = 'v1'):
+    def create(eventID: str, accessToken: str, expiry:str, version: str = 'v1'):
         """
         Initializes client object with Hubb.me API endpoint data. Use Postman to get your accessToken.
         :param eventID: aka 'scope' or a value which represents which Hubb site this client works on
@@ -57,7 +17,7 @@ class pyhubbcli:
         :param version: API version, set by default to v1
         """
         apiclient = pyhubb.client(eventID, accessToken, 'bearer', expiry, version)
-        save_object(apiclient)
+        
     def request(fields: str, section: str = 'Sessions', query: str = 'expand'):
         """
         Make requests to the Hubb API at various endpoints (or sections) with various OData query parameters and delimiting fields
@@ -65,8 +25,7 @@ class pyhubbcli:
         :param section: or endpoint. The part of the Hubb site you wish to get data from. Some examples are: Sessions, Locations, Users, Sponsors, etc. More can be found at https://ngapi.hubb.me/swagger/ui/index
         :param query: Options which allow you to 'expand' a data field for details, 'filter' the data to what you want to see, 'select' to limit data fields returned, 'order' data by certain parameters, or limit the data to the 'top' X results (put your number in the fields parameter)
         """
-        exists('client.pickle')
-        apiclient = load_object('client.pickle')
+        pyhubb.client.expand(fields, section)
 
 if __name__ == "__main__":
     fire.core.Display = lambda lines, out: print(*lines, file=out) #hacky solution to get fire to print to stdout
